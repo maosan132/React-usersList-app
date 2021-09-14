2676e064dece6f1de214506dfb0c7bf82f353887
maosan132 <maosan132@gmail.com>
2021-09-14 17:22:39 -0500

    Add event handlers

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import { useState } from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+  
   const addUserHandler = (e) => {
     e.preventDefault();
+  }
 
+  const usernameChangerHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  }
+
+  const ageChangerHandler = (e) => {
+    setEnteredAge(e.target.value);
   }
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
+        <input type="text" id="username" conChange={usernameChangerHandler}/>
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageChangerHandler}/>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
