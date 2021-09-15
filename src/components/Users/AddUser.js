3ca801fd065a6419faec6640c34cf8744ca86ebd
maosan132 <maosan132@gmail.com>
2021-09-15 09:23:11 -0500

    Add errorHandler to AddUser

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -46,7 +46,7 @@ const AddUser = (props) => {
 
   return (
     <div>
-      { error && <ErrorModal title={error.title} message={error.message}/> }
+      { error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/> }
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
