da6c909027a1bf541dd6f31f459a02a061adc56b
maosan132 <maosan132@gmail.com>
2021-09-15 11:05:51 -0500

    Add fragment

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,7 +3,7 @@ import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
-// import Wrapper from '../Helpers/Wrapper';
+import Wrapper from '../Helpers/Wrapper';
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
@@ -46,8 +46,7 @@ const AddUser = (props) => {
   }
 
   return (
-    //<Wrapper>
-    <>
+    <Wrapper>
       { error && <
         ErrorModal
         title={error.title}
@@ -74,8 +73,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </>
-    //</Wrapper>
+    </Wrapper>
   )
 }
 
