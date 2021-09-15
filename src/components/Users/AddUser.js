7c6726a7ceca677a777abf003bb07fb96f223d1c
maosan132 <maosan132@gmail.com>
2021-09-15 16:11:15 -0500

    Use useRef to fetch and clear data in inputs

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
@@ -6,20 +6,25 @@ import ErrorModal from '../UI/ErrorModal';
 import Wrapper from '../Helpers/Wrapper';
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  // const [enteredUsername, setEnteredUsername] = useState(''); // NO longer needed after useRef
+  // const [enteredAge, setEnteredAge] = useState('');
   const [error, setError] = useState()
 
   const addUserHandler = (e) => { // Validation
     e.preventDefault();
-    if (enteredAge.trim().lenght === 0 || enteredUsername.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+    if (enteredUserAge.trim().lenght === 0 || enteredName.trim().length === 0) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid username and age'
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age greater than 0'
@@ -27,26 +32,27 @@ const AddUser = (props) => {
       return;
     }
 
-    props.onAddUser(enteredUsername, enteredAge);
-    console.log();
-    setEnteredUsername('');
-    setEnteredAge('');
+    props.onAddUser(enteredName, enteredUserAge);
+    // setEnteredUsername(''); // No needed because we don't need to update the state after useRef
+    // setEnteredAge('');
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
   }
 
-  const usernameChangerHandler = (e) => {
-    setEnteredUsername(e.target.value);
-  }
+  // const usernameChangerHandler = (e) => {  // NO needed after useRef
+  //   setEnteredUsername(e.target.value);
+  // }
 
-  const ageChangerHandler = (e) => {
-    setEnteredAge(e.target.value);
-  }
+  // const ageChangerHandler = (e) => {  // NO needed after useRef
+  //   setEnteredAge(e.target.value);
+  // }
 
   const errorHandler = () => {
     setError(null);
   }
 
   return (
-    <Wrapper>
+    <Wrapper> { /* It could also be <>*/ }
       { error && <
         ErrorModal // error modal component
         title={error.title}
@@ -60,15 +66,17 @@ const AddUser = (props) => {
           <input
             type="text"
             id="username"
-            onChange={usernameChangerHandler}
-            value={enteredUsername}
+            // onChange={usernameChangerHandler}  // NO needed after useRef
+            // value={enteredUsername}
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            onChange={ageChangerHandler}
-            value={enteredAge}
+            // onChange={ageChangerHandler}  // NO needed after useRef
+            // value={enteredAge}
+            ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
