b793e5771efded71d7bc0e5536a0891c8b133532
maosan132 <maosan132@gmail.com>
2021-09-14 23:26:20 -0500

    Add Button and Card to errorModal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import Card from './components/UI/Card';
+import Button from './components/UI/Button';
 
 const ErrorModal = (props) => {
   return (
-    <div>
-      
-    </div>
+    <Card>
+      <header>
+        <h2>{props.title</h2>
+      </header>
+      <div>
+        <p>{props.message}</p>
+      </div>
+      <footer>
+        <Button onClick={props.onClear}>Ok</Button>
+      </footer>
+    </Card>
   )
 }
 
