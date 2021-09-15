4e5d23e37be3a241723f7e01e366bd0000354be0
maosan132 <maosan132@gmail.com>
2021-09-14 22:51:36 -0500

    Move userslist to App.js

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,8 +1,9 @@
-import Card from './UI/Card'
+import Card from '../UI/Card'
+import classes from './UsersList.module.css'
 
 const UsersList = (props) => {
   return (
-    <Card>
+    <Card className={classes.users}>
     <ul>
       {props.users.map(u => (
       <li
